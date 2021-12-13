2181a38548ec51e7cd2552ef1c86e5dc1270f7b9
Vladimir Iljin <oivt@ya.ru>
2021-12-13 18:54:32 +0300

    18

--- src/com/company/View.java
+++ src/com/company/View.java
@@ -7,7 +7,7 @@ public class View {
     void showLocation(Location location) {
         System.out.println(location.getText());
         Action[] actions = location.getActions();
-        for (int i = 0; i < actions.length; ++i){
+        for (int i = 0; i < actions.length; ++i) {
             System.out.println(i + 1 + ") "+ actions[i].getText());
         }
     }
