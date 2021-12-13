45af079898d7e82afb1ea5c59416b86bd2d9de67
Vladimir Iljin <oivt@ya.ru>
2021-12-13 18:44:01 +0300

    18

--- src/com/company/View.java
+++ src/com/company/View.java
@@ -4,17 +4,11 @@ import java.util.Scanner;
 
 public class View {
     static Scanner in = new Scanner(System.in);
-    UserChoiceListener ucl;
-    View(UserChoiceListener ucl){
-        this.ucl= ucl;
-    }
-    void showLocation(Location location){
+    void showLocation(Location location) {
         System.out.println(location.getText());
         Action[] actions = location.getActions();
         for (int i = 0; i < actions.length; ++i){
             System.out.println(i + 1 + ") "+ actions[i].getText());
         }
-        int ans = in.nextInt() - 1;
-        ucl.userChoice(ans);
     }
 }
